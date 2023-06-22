95ada174bdc8f011c04b6cc72b8af8284dbe2520
Developer <developer@repeat-7c:24:99:f3:30:73.speedport.ip>
2023-06-22 11:23:18 +0200

    init of components summary, board and contacts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SummaryComponent } from './summary/summary.component';
+import { BoardComponent } from './board/board.component';
+import { ContactsComponent } from './contacts/contacts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SummaryComponent,
+    BoardComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule,
